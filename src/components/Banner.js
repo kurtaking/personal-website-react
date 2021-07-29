83953c8f6181fd07ce3f817402dca5577d40a724
Kurt King <kurtaking@gmail.com>
2021-07-29 16:05:37 -0500

    resized banner to fit the screen better

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,9 +2,13 @@ import logo from "../kkwhite-blue1.png";
 
 const Banner = () => {
   return (
-    <div className="flex justify-center object-fill mx-auto bg-kkblue">
-      <div className="w-full md:w-2/3 lg:w-1/2">
-        <img src={logo} alt="Kurt Logo" />
+    <div className="flex justify-center mx-auto bg-kkblue">
+      <div className="flex justify-center w-full md:w-2/3 lg:w-1/2">
+        <img
+          src={logo}
+          alt="Kurt Logo"
+          className="object-contain h-64 items-center"
+        />
       </div>
     </div>
   );
